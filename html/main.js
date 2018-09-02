1cc7d5d63642442e6651545ac38600c77e7c8208
KDyeon <do.yeon1290@gmail.com>
2018-09-02 17:02:17 +0900

    180902-bizcard

---
+++ html/main.js
@@ -0,0 +1,9 @@
+'use strict'
+
+function on() {
+    document.getElementById("container-fluid").style.display = "block";
+}
+
+function off() {
+    document.getElementById("container-fluid").style.display = "none";
+}
